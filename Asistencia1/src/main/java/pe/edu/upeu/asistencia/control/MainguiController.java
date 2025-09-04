081ba41ffa8fc9a93fc92a0f88140da275f5d45f
Cerezo_dev <unacuentamasuwux@gmail.com>
2025-09-04 10:20:11 -0500

    KAS PRATICA

--- Asistencia1/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
+++ Asistencia1/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
@@ -1,15 +1,19 @@
 package pe.edu.upeu.asistencia.control;
-
-
+import javafx.application.Platform;
+import javafx.scene.control.TabPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.control.Tab;
+
+import javafx.scene.control.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 
+import java.util.Map;
 
 
 @Controller
@@ -21,40 +25,64 @@ public class MainguiController {
     private MenuBar menuBar;
     @FXML
     private TabPane tabPane;
+
     @Autowired
     private ApplicationContext context;
+
     @FXML
     public void initialize(){
-
+        MEnuIemListener miL=new MEnuIemListener();
+        menuItem1.setOnAction(miL::handle);
+        menuItem2.setOnAction(miL::handle);
     }
+
+
      class MEnuIemListener {
 
+         Map<String, String[]> menuConfig = Map.of(
+                 "menuItem1", new String[]{"/fxml/main_participante.fxml", "Participante", "T"},
+                 "menuItem2", new String[]{"/fxml/login.fxml", "salir", "C"}
 
-         public void handle(ActionEvent event) {
+         );
+
+
+         public void handle(ActionEvent e) {
+             System.out.println("sdsdsds");
+             String id = ((MenuItem) e.getSource()).getId();
+             System.out.println(id);
+             if (menuConfig.containsKey(id)) {
+                 String[] mi = menuConfig.get(id);
+                 if (mi[2].equals("C")) {
+                     Platform.exit();
+                     System.exit(0);
+
+                 } else {
+                     abrirArchivoFML(mi[0], mi[1]);
+                 }
+             }
          }
 
-         public void abrirArchivoFML(String filename, String tittle){
-          try {
 
-              FXMLLoader loader = new FXMLLoader(getClass().getResource(filename));
-              loader.setControllerFactory(context::getBean);
-              Parent root = loader.load();
+         public void abrirArchivoFML(String filename, String tittle) {
+             try {
 
-              ScrollPane scrollPane = (ScrollPane) root;
-              scrollPane.setFitToHeight(true);
-              scrollPane.setFitToHeight(true);
-              Tab newTab=new Tab(tittle,   scrollPane);
-              tabPane.getTabs().add(newTab);
-              tabPane.getTabs().add(newTab);
-          } catch (Exception e) {
-              throw new RuntimeException(e);
-          }
+                 FXMLLoader loader = new FXMLLoader(getClass().getResource(filename));
+                 loader.setControllerFactory(context::getBean);
+                 Parent root = loader.load();
+
+                 ScrollPane scrollPane = new ScrollPane(root);
+                 scrollPane.setFitToHeight(true);
+                 scrollPane.setFitToHeight(true);
+                 Tab newTab = new Tab(tittle, scrollPane);
+                 tabPane.getTabs().clear();
+                 tabPane.getTabs().add(newTab);
+             } catch (Exception e) {
+                 throw new RuntimeException(e);
+             }
 
 
-
-
+         }
      }
-
      class MenuListener{
         public void handle (ActionEvent event){
 
